16c2cb938197115daf1efc8e20cd112a3c9af582
percival1101 <thaonek@gmail.com>
2025-04-23 18:16:04 +0700

    set defaut

--- @latest/src/helpers/api.js
+++ @latest/src/helpers/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+axios.defaults.baseURL = 'https://readw.onrender.com/'; // 'http://localhost:5000/'
+
 const backEnd = 'https://readw.onrender.com/' // 'http://localhost:5000/'
 
 export const saveFullStory = async () => {
